e29970ebd909e8ebc02c1190fad2c0f0b46f627e
NikSkor <mr-zerx@yandex.ru>
2022-07-21 03:39:42 +0500

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi dude!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
