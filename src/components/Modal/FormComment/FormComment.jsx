83b9949a07f3a2c239a1e569e66f8bb79abc74dd
NikSkor <mr-zerx@yandex.ru>
2022-09-06 21:46:13 +0500

    focus added

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -6,7 +6,16 @@ import {useRef, useEffect, useState} from 'react';
 
 export const FormComment = () => {
   const confirmRef = useRef(null);
+  const textAreaRef = useRef(null);
   const [textState, setTextState] = useState('');
+  const [isOpenButton, setIsOpenButton] = useState(true);
+  const [isOpenComment, setIsOpenComment] = useState(false);
+
+  const handleComment = e => {
+    e.preventDefault();
+    setIsOpenButton(false);
+    setIsOpenComment(true);
+  };
 
   const handleChange = e => {
     const target = e.target;
@@ -21,18 +30,33 @@ export const FormComment = () => {
     }
   };
 
+  const handleFocus = e => {
+    textAreaRef.current.focus();
+  };
+
   useEffect(() => {
     document.addEventListener('click', handleClick);
+    document.addEventListener('click', handleFocus);
     return () => {
       document.removeEventListener('click', handleClick);
+      document.removeEventListener('click', handleFocus);
     };
   });
 
   return (
-    <form className={style.form}>
-      <Text as='h3' size={14} tsize={18}>Имя авторизованного пользователя</Text>
-      <textarea className={style.textarea} onChange={handleChange}></textarea>
-      <button className={style.btn} ref={confirmRef}>Отправить</button>
-    </form>
+    <>
+      {isOpenButton &&
+        <button className={style.btn + ' ' + style.btnRight}
+          onClick={handleComment}>Написать комментарий</button>}
+      {isOpenComment &&
+        <form className={style.form}>
+          <Text as='h3' size={14} tsize={18}>
+            Имя авторизованного пользователя</Text>
+          <textarea className={style.textarea} onChange={handleChange}
+            ref={textAreaRef}></textarea>
+          <button className={style.btn} ref={confirmRef}>Отправить</button>
+        </form>
+      }
+    </>
   );
 };
